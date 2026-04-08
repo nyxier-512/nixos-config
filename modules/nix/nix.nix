6cae70728d4dec90689472ce83f28b26f343be98
NyXieR-648074 <kyvd.god@gmail.com>
2026-04-08 16:57:46 +0200

    switched to lix

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -1,4 +1,4 @@
-{ username, inputs, ... }:
+{ username, inputs, pkgs, ... }:
 
 {
   nix = {
@@ -13,4 +13,13 @@
       ];
     };
   };
+    nixpkgs.overlays = [ (final: prev: {
+    inherit (prev.lixPackageSets.stable)
+      nixpkgs-review
+      nix-eval-jobs
+      nix-fast-build
+      colmena;
+  }) ];
+
+  nix.package = pkgs.lixPackageSets.stable.lix;
 }
